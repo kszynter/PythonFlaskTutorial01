db87a949d926e2814d8e2f22c270659b0941af35
Krzysztof Szynter <kszynter@gmail.com>
2017-01-07 22:15:03 +0100

    mapping custom URLs

--- main.py
+++ main.py
@@ -8,9 +8,21 @@ def index():
     return 'This is the homepage'
 
 
+# decorator maps an URL to return value
 @app.route('/about')
 def about():
     return 'This is the about page'
+
+
+# passing a variable value in the URL
+@app.route('/profile/<username>')
+def profile(username):
+    return 'Hello %s' % username
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return 'Post id is: %s' % post_id
 
 
 # run the app only when this script is run directly
